b6c172571f50d6c0b29d223d6c51cd13596b8ba7
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-01 12:04:54 -0300

    Add files via upload

---
+++ PrimeiroAppSwiftUI/PrimeiroAppSwiftUI/PrimeiroAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroAppSwiftUIApp.swift
+//  PrimeiroAppSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
